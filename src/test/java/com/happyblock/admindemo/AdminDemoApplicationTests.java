6ee6e96b176aa80ae5c96a6583386ea836e51c4d
용길한 <gilhan.yong@happyblock.io>
2026-01-29 16:38:30 +0900

    first commit

---
+++ src/test/java/com/happyblock/admindemo/AdminDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyblock.admindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
